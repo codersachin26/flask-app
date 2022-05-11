9dcdbb9c1eb8fb0e5adb807ea8cffc8a97f622e7
codersachin26 <sachinindoriya63@gmail.com>
2022-05-11 23:17:08 +0530

    new msg added

--- application.py
+++ application.py
@@ -3,11 +3,8 @@ application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Your Flask App Works!"
+    return "Hello Friends"
 
-@application.route("/hello")
-def hello():
-    return "Hello Friend Again!!"
 
 
 if __name__ == "__main__":
